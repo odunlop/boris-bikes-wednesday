e5a911207453c2c274462be76fef12ef0d1653dd
Orla <orladunlop2@gmail.com>
2021-12-14 15:28:50 +0000

    Added rspec test for if the released bike is working? added content to release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,10 @@
+require_relative 'bike'
+
 class DockingStation
 
 def release_bike
+  bike = Bike.new
+  return bike
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,14 @@ require 'docking_station'
 
 describe DockingStation do 
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
+
+# Add a test to your DockingStation specification that 
+# a) gets a bike, and then 
+# b) expects the bike to be working
